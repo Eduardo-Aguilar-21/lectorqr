a1d945ade2417d93b5c5c523f6d948cbd79f2cb6
Eduardo Aguilar <es123y123@gmail.com>
2023-07-21 12:53:06 -0500

    COdigo qr

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+const QRScanner = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
 
-export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      setHasCameraPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ data }) => {
+    setScanned(true);
+    alert(`Código QR escaneado: ${data}`);
+  };
+
+  if (hasCameraPermission === null) {
+    return <View />;
+  }
+  if (hasCameraPermission === false) {
+    return <Text>No se ha otorgado el permiso para acceder a la cámara.</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.preview}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      />
+      {scanned && (
+        <TouchableOpacity onPress={() => setScanned(false)} style={styles.scanAgain}>
+          <Text style={styles.scanAgainText}>Escanear de nuevo</Text>
+        </TouchableOpacity>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  scanAgain: {
+    padding: 15,
+    backgroundColor: 'white',
+    margin: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  scanAgainText: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
+
+export default QRScanner;
